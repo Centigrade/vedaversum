31fd0c900ae75f9fd8acd6044d1a6343ca2909ac
Ulrike Kulzer <ulrike.kulzer@HW1179.CENTIGRADE>
2022-05-31 14:49:43 +0200

    restructured project

--- VedaVersum.Frontend/src/views/ArticleDetailsView.tsx
+++ VedaVersum.Frontend/src/views/ArticleDetailsView.tsx
@@ -1,7 +1,6 @@
 import { useQuery } from '@apollo/client';
 import { useParams } from 'react-router-dom';
 import { ARTICLE_BY_ID_QUERY } from '../api/articles-queries';
-import { RequireAuth } from '../authentication/RequreAuth';
 import { GetArticle } from '../model/get-article-by-id-response';
 import ArticlesItem from './components/ArticleItem';
 import UserList from './components/UserList';
@@ -21,23 +20,21 @@ function ArticleDetailsView() {
 
   //#region render view
   return (
-    <RequireAuth>
-      <div className="d-flex">
-        {/* article details */}
-        <div className="px-4 py-3 w-75">
-          {loading && <p>Loading...</p>}
-          {error && <p>{error.message} :(</p>}
-          {!data && <p>No data available</p>}
-          {currentArticle && (
-            <div className="">
-              <ArticlesItem articleData={currentArticle} preview={false} />
-            </div>
-          )}
-        </div>
-        {/* users list */}
-        <UserList />
+    <div className="d-flex">
+      {/* article details */}
+      <div className="px-4 py-3 w-75">
+        {loading && <p>Loading...</p>}
+        {error && <p>{error.message} :(</p>}
+        {!data && <p>No data available</p>}
+        {currentArticle && (
+          <div className="">
+            <ArticlesItem articleData={currentArticle} preview={false} />
+          </div>
+        )}
       </div>
-    </RequireAuth>
+      {/* users list */}
+      <UserList />
+    </div>
   );
   //#endregion
 }
