411c325f526e6ca0b5e22e15748f50421fc75a28
Ulrike Kulzer <ulrike.kulzer@centigrade.de>
2022-06-29 11:52:31 +0200

    chore: code clean up

--- VedaVersum.Frontend/src/views/components/UserName.tsx
+++ VedaVersum.Frontend/src/views/components/UserName.tsx
@@ -1,24 +1,22 @@
 import { getAvatarUrl } from 'utils/main';
 
-//#region user name properties
+// user name properties
 export interface UsernameProps {
   email: string;
 }
-//#endregion
 
 function UserName(props: UsernameProps) {
   // get user name from the user e-mail
   const userName = props.email.split('@')[0];
   const preparedName = userName.split('.')[0];
 
-  //#region render component
+  // render component
   return (
     <div className="flex items-center">
       <img className="w-6 rounded-full mr-2 border border-primary" src={getAvatarUrl(userName)} alt="some pic" />
       <span className="text-primary mr-2">{preparedName}</span>
     </div>
   );
-  //#endregion
 }
 
 export default UserName;
